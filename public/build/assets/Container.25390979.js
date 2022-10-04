022a4920b298e9f84fb2fb303d7c35baa457025c
maesa <sysdev3rsbm@rsbm.com>
2022-10-04 22:37:53 +0800

    perbaikan landing page

---
+++ public/build/assets/Container.25390979.js
@@ -0,0 +1,1 @@
+import{a as t}from"./app.e44ecba3.js";function r({children:a}){return t("div",{className:"px-1 mx-auto bg-white",children:a})}export{r as C};
---
+++ public/build/assets/use-computed.8d07558a.js
@@ -0,0 +1,1 @@
+import{r as s}from"./app.e44ecba3.js";import{a as u,t as i}from"./Tawarin.70053fc7.js";function p(r,o){let[a,t]=s.exports.useState(r),e=u(r);return i(()=>t(e.current),[e,t,...o]),a}export{p as i};
