df7476094a4d9c9d5713b19eba3f1019a0289654
maesa <sysdev3rsbm@rsbm.com>
2022-10-09 14:46:31 +0800

    Perbaikan kecil di main branch npm run build

---
+++ public/build/assets/Container.9c1e4601.js
@@ -0,0 +1,1 @@
+import{a as t}from"./app.a20c4654.js";function r({children:a}){return t("div",{className:"px-4 mx-auto bg-white",children:a})}export{r as C};
---
+++ public/build/assets/use-computed.d66f1183.js
@@ -0,0 +1,1 @@
+import{r as s}from"./app.a20c4654.js";import{a as u,t as i}from"./XIcon.11a08d41.js";function p(r,o){let[a,t]=s.exports.useState(r),e=u(r);return i(()=>t(e.current),[e,t,...o]),a}export{p as i};
