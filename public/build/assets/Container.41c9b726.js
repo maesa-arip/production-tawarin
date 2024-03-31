4cfe30255a5f90ad99e3d7f0a5598f6e9378d1b0
maesa <sysdev3rsbm@rsbm.com>
2024-03-31 13:22:23 +0800

    testing logo not using Link

---
+++ public/build/assets/Container.41c9b726.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app.ce08ded8.js";function a({children:m}){return r("div",{className:"mx-2 bg-white md:mx-6",children:m})}export{a as C};
--- resources/js/Layouts/Header.jsx
+++ resources/js/Layouts/Header.jsx
@@ -194,7 +194,7 @@ export default function Header() {
                 <div className="px-4 mx-auto sm:px-6">
                     <div className="flex items-center justify-between py-6 border-b-2 border-gray-100 md:justify-start md:space-x-10">
                         <div className="flex justify-start">
-                            <NavLink href="/">
+                            <a href="/">
                                 <span className="sr-only"></span>
                                 <img
                                     className="w-8 h-8"
@@ -202,7 +202,7 @@ export default function Header() {
                                     aria-label="TawarinLogo"
                                     alt="Logo"
                                 />
-                            </NavLink>
+                            </a>
                         </div>
                         {auth.user ? (
                             <>
